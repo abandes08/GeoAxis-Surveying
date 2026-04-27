8f1cbd227af8ec88bcd895afaa839a9792191a89
abandes08 <andesanthony08@gmail.com>
2026-04-27 15:09:26 +0800

    fix: fix vercel analytics import from Nextjs to React

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import { Helmet } from 'react-helmet-async'
 
 import Navbar from './components/Navbar'
